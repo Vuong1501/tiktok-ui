20a227dd471e6511ee2ed45f32263199ffba4bb8
Vuong1501 <vuong.44.10a5@gmail.com>
2025-05-09 09:36:59 +0700

    Xây dựng UI Header phần 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,10 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 import styles from './Header.module.scss';
-
+import images from '~/assets/images';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -9,6 +12,31 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+
+                <img src={images.logo} alt="tiktok" className={cx('logo-img')} />
+
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts and videos' spellCheck={false} />
+                    <button className={cx('clear')}>
+                        {/* Clear */}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+
+                    </button>
+
+                    {/* Loading icon */}
+                    <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        {/* Search icon */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+
+                    </button>
+                </div>
+
+                <div className={cx('actions')}>
+
+
+                </div>
             </div>
         </header>
 
